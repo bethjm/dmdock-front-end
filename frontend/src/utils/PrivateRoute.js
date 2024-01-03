1e33d7bcacaf180fb77ac242f930fe78af8f0692
bethjm <b.jeanne.mills@gmail.com>
2024-01-02 22:56:40 -0500

    adding new baseurl

--- frontend/src/utils/PrivateRoute.js
+++ frontend/src/utils/PrivateRoute.js
@@ -2,7 +2,6 @@ import { Route, Redirect } from "react-router-dom";
 import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 
-
 const PrivateRoute = ({ children, ...rest }) => {
   let { user } = useContext(AuthContext);
   return <Route {...rest}>{!user ? <Redirect to="/login" /> : children}</Route>;
